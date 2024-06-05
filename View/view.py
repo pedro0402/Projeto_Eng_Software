c16b0321b2fe26aae1b4630f05842761976a43ad
pedro0402 <pedro693.ph@gmail.com>
2024-06-04 23:46:07 -0300

    Criação das funções validate_quantity & create_manage_inventory_screen & create_product_list

--- View/view.py
+++ View/view.py
@@ -10,7 +10,7 @@ class InventoryView(ctk.CTk):
         self.geometry("800x600")
 
         self.create_home_screen()
-        
+
     def create_home_screen(self):
         self.clear_screen()
 
@@ -49,5 +49,48 @@ class InventoryView(ctk.CTk):
 
         self.back_button = ctk.CTkButton(self, text="Voltar", command=self.create_home_screen)
         self.back_button.pack(pady=10)
+        
+    def validate_quantity(self, new_value):
+        if new_value.isdigit() or new_value == "":
+            return True
+        return False
+
+    def create_manage_inventory_screen(self):
+        self.clear_screen()
+
+        self.products_frame = ctk.CTkFrame(self)
+        self.products_frame.pack(pady=10, padx=20, fill="both", expand=True)
+
+        self.create_product_list()
+
+        self.action_frame = ctk.CTkFrame(self)
+        self.action_frame.pack(pady=10)
+
+        self.remove_button = ctk.CTkButton(self.action_frame, text="Retirar Produto", command=self.show_remove_product_window)
+        self.remove_button.pack(side="left", padx=10)
+
+        self.delete_button = ctk.CTkButton(self.action_frame, text="Excluir Produto", command=self.show_delete_product_window)
+        self.delete_button.pack(side="right", padx=10)
+
+        self.back_button = ctk.CTkButton(self, text="Voltar", command=self.create_home_screen)
+        self.back_button.pack(pady=10)
+
+
+    def create_product_list(self):
+        products = self.controller.get_all_products()
+        self.products_table = ctk.CTkScrollableFrame(self.products_frame)
+        self.products_table.pack(fill="both", expand=True, padx=10, pady=10)
+
+        headers = ["ID", "Nome", "Quantidade", "Setor"]
+        for i, header in enumerate(headers):
+            header_label = ctk.CTkLabel(self.products_table, text=header, font=("Arial", 14, "bold"))
+            header_label.grid(row=0, column=i, padx=5, pady=5)
+
+        for row_num, product in enumerate(products, start=1):
+            for col_num, detail in enumerate(product):
+                detail_label = ctk.CTkLabel(self.products_table, text=detail, font=("Arial", 12))
+                detail_label.grid(row=row_num, column=col_num, padx=5, pady=5)
+                if col_num == 0:  # ID column
+                    detail_label.bind("<Button-1>", lambda e, text=detail: self.copy_to_clipboard(text))
 
     
