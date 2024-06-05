b31b03970f575e61698f52fc2bb2813d9f304bb2
pedro0402 <pedro693.ph@gmail.com>
2024-06-04 23:41:59 -0300

    Criação do View

---
+++ View/view.py
@@ -0,0 +1,13 @@
+
+
+class InventoryView(ctk.CTk):
+    def __init__(self, controller):
+        super().__init__()
+
+        self.controller = controller
+        self.title("STOCK SEC - Gerenciador de Estoque")
+        self.geometry("800x600")
+
+        self.create_home_screen()
+
+    
