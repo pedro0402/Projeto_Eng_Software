9736de530f22b2f3e3d24a0b7d98961d7b656376
Lucas Patricio <lucaspatricio.8@gmail.com>
2024-06-04 23:02:57 -0300

    Criação das funções de deletar o produto por nome, deletar por id, pegar todos os produtos e função run

--- Controller/controller.py
+++ Controller/controller.py
@@ -5,7 +5,7 @@ class InventoryController:
     def __init__(self):
         self.model = InventoryModel()
         self.view = InventoryView(self)
-        
+
     def add_product(self, name, quantity, sector):
         self.model.add_product(name, quantity, sector)
 
@@ -14,3 +14,15 @@ class InventoryController:
 
     def subtract_product_quantity_by_id(self, product_id, quantity):
         self.model.subtract_product_quantity_by_id(product_id, quantity)
+
+    def delete_product(self, name):
+        self.model.delete_product(name)
+
+    def delete_product_by_id(self, product_id):
+        self.model.delete_product_by_id(product_id)
+
+    def get_all_products(self):
+        return self.model.get_all_products()
+
+    def run(self):
+        self.view.mainloop()
