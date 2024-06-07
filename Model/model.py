179a3413bc68053c60cea81f5a925e01800dc63c
pedro0402 <pedro693.ph@gmail.com>
2024-06-07 00:38:32 -0300

    criação da função para exportar para excel

--- Model/model.py
+++ Model/model.py
@@ -1,5 +1,7 @@
 import sqlite3
 import random
+from datetime import datetime
+import pandas as pd
 
 class InventoryModel:
     def __init__(self, db_name="database.db"):
@@ -17,6 +19,20 @@ class InventoryModel:
         """
         self.conn.execute(query)
         self.conn.commit()
+        
+        # Adiciona uma tabela para registrar o histórico de adições
+        query = """
+        CREATE TABLE IF NOT EXISTS history (
+            id INTEGER PRIMARY KEY,
+            product_id INTEGER NOT NULL,
+            operation TEXT NOT NULL,
+            timestamp TEXT NOT NULL
+        )
+        """
+        self.conn.execute(query)
+        self.conn.commit()
+
+
 
     def generate_unique_id(self):
         while True:
@@ -44,7 +60,17 @@ class InventoryModel:
             product_id = self.generate_unique_id()
             query = "INSERT INTO products (id, name, quantity, sector) VALUES (?, ?, ?, ?)"
             self.conn.execute(query, (product_id, name, quantity, sector))
+        
+        # Registra a operação de adição no histórico
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        query = "INSERT INTO history (product_id, operation, timestamp) VALUES (?, ?, ?)"
+        self.conn.execute(query, (product_id, "Adição", timestamp))
         self.conn.commit()
+
+    def get_product_addition_history(self):
+        query = "SELECT * FROM history WHERE operation = 'Adição'"
+        cursor = self.conn.execute(query)
+        return cursor.fetchall()
 
     def subtract_product_quantity(self, name, quantity):
         product = self.get_product_by_name(name)
@@ -81,3 +107,10 @@ class InventoryModel:
         query = "SELECT * FROM products"
         cursor = self.conn.execute(query)
         return cursor.fetchall()
+    
+    def export_to_excel(self, file_path):
+        query = "SELECT * FROM products"
+        data = pd.read_sql(query, self.conn)
+        data.to_excel(file_path, index=False)
+
+inventory = InventoryModel()
