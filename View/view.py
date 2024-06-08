3a786ac61186e9b5599748616b597e9b2f12e808
Paulo Vieira <paulovieira67643@outlook.com>
2024-06-08 16:39:19 -0300

    Adicionado a função de filtrar por setor na gestão de estoque.

--- View/view.py
+++ View/view.py
@@ -58,6 +58,21 @@ class InventoryView(ctk.CTk):
     def create_manage_inventory_screen(self):
         self.clear_screen()
 
+        self.filter_frame = ctk.CTkFrame(self)
+        self.filter_frame.pack(pady=10)
+
+        self.back_button = ctk.CTkButton(self.filter_frame, text="Voltar", command=self.create_home_screen)
+        self.back_button.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+
+        self.filter_label = ctk.CTkLabel(self.filter_frame, text="Filtrar por Setor:")
+        self.filter_label.grid(row=0, column=1, padx=5, pady=5)
+
+        self.filter_var = ctk.StringVar()
+        self.filter_options = ["Todos"] + self.controller.model.get_all_sectors()
+        self.filter_var.set("Todos")
+        self.filter_menu = ctk.CTkOptionMenu(self.filter_frame, values=self.filter_options, variable=self.filter_var, command=self.apply_filter)
+        self.filter_menu.grid(row=0, column=2, padx=5, pady=5)
+
         self.products_frame = ctk.CTkFrame(self)
         self.products_frame.pack(pady=10, padx=20, fill="both", expand=True)
 
@@ -74,12 +89,7 @@ class InventoryView(ctk.CTk):
 
         self.show_export_excel_button()
 
-        self.back_button = ctk.CTkButton(self, text="Voltar", command=self.create_home_screen)
-        self.back_button.pack(pady=10)
-
-
     def create_product_list(self):
-        products = self.controller.get_all_products()
         self.products_table = ctk.CTkScrollableFrame(self.products_frame)
         self.products_table.pack(fill="both", expand=True, padx=10, pady=10)
 
@@ -88,59 +98,38 @@ class InventoryView(ctk.CTk):
             header_label = ctk.CTkLabel(self.products_table, text=header, font=("Arial", 14, "bold"))
             header_label.grid(row=0, column=i, padx=5, pady=5)
 
+        self.update_product_list()
+
+    def update_product_list(self):
+        sector_filter = self.filter_var.get()
+        products = self.controller.get_all_products()
+
+        if sector_filter != "Todos":
+            products = [product for product in products if product[3] == sector_filter]
+
+        self.clear_product_list()
+
         for row_num, product in enumerate(products, start=1):
             for col_num, detail in enumerate(product):
-                if col_num == 2:  # Check if it's the quantity column
+                if col_num == 2:  # Quantidade column
                     if detail == 0:
-                        detail = "Fora de Estoque"  # Replace 0 with "Fora de Estoque"
+                        detail = "Fora de Estoque"
                 detail_label = ctk.CTkLabel(self.products_table, text=detail, font=("Arial", 12))
                 detail_label.grid(row=row_num, column=col_num, padx=5, pady=5)
                 if col_num == 0:  # ID column
                     detail_label.bind("<Button-1>", lambda e, text=detail: self.copy_to_clipboard(text))
 
+    def clear_product_list(self):
+        for widget in self.products_table.winfo_children():
+            widget.destroy()
 
     def copy_to_clipboard(self, text):
         self.clipboard_clear()
         self.clipboard_append(text)
         messagebox.showinfo("ID Copiado", f"ID {text} copiado para a área de transferência!")
 
-    def show_remove_product_window(self):
-        self.remove_window = ctk.CTkToplevel(self)
-        self.remove_window.title("Retirar Produto")
-        self.remove_window.geometry("400x200+{}+{}".format(self.winfo_x() + self.winfo_width() // 2 - 200, self.winfo_y() + self.winfo_height() // 2 - 100))
-        self.remove_window.transient(self)  # Define a janela principal como a janela mestra
-        self.remove_window.grab_set()  # Impede interação com a janela principal enquanto esta estiver aberta
-
-        self.remove_id_label = ctk.CTkLabel(self.remove_window, text="ID do Produto:")
-        self.remove_id_label.pack(pady=5)
-        self.remove_id_entry = ctk.CTkEntry(self.remove_window)
-        self.remove_id_entry.pack(pady=5)
-
-        self.remove_quantity_label = ctk.CTkLabel(self.remove_window, text="Quantidade a Retirar:")
-        self.remove_quantity_label.pack(pady=5)
-        self.remove_quantity_entry = ctk.CTkEntry(self.remove_window)
-        self.remove_quantity_entry.pack(pady=5)
-
-        self.remove_confirm_button = ctk.CTkButton(self.remove_window, text="Confirmar", command=self.remove_product)
-        self.remove_confirm_button.pack(pady=10)
-
-
-
-    def show_delete_product_window(self):
-        self.delete_window = ctk.CTkToplevel(self)
-        self.delete_window.title("Excluir Produto")
-        self.delete_window.geometry("400x150+{}+{}".format(self.winfo_x() + self.winfo_width() // 2 - 200, self.winfo_y() + self.winfo_height() // 2 - 75))
-        self.delete_window.transient(self)  # Define a janela principal como a janela mestra
-        self.delete_window.grab_set()  # Impede interação com a janela principal enquanto esta estiver aberta
-
-        self.delete_id_label = ctk.CTkLabel(self.delete_window, text="ID do Produto:")
-        self.delete_id_label.pack(pady=5)
-        self.delete_id_entry = ctk.CTkEntry(self.delete_window)
-        self.delete_id_entry.pack(pady=5)
-
-        self.delete_confirm_button = ctk.CTkButton(self.delete_window, text="Confirmar", command=self.delete_product)
-        self.delete_confirm_button.pack(pady=10)
-
+    def apply_filter(self, *args):
+        self.update_product_list()
 
     def add_product(self):
         name = self.name_entry.get().strip()
@@ -197,22 +186,16 @@ class InventoryView(ctk.CTk):
         self.export_button = ctk.CTkButton(self.action_frame, text="Exportar Excel", command=self.export_to_excel)
         self.export_button.pack(side="right", padx=16)
 
-
     def export_to_excel(self, file_path="inventory.xlsx"):
         try:
-            # Verifica se o caminho do arquivo é válido
             if not file_path:
                 raise ValueError("Caminho do arquivo não fornecido.")
 
-            # Chama o método export_excel do controlador
             self.controller.export_excel(file_path)
 
-            # Exibe uma mensagem de confirmação
             messagebox.showinfo("Exportação Concluída", f"Os dados foram exportados para {file_path} com sucesso.")
         except Exception as e:
-            # Em caso de erro, exibe uma mensagem de erro
             messagebox.showerror("Erro na Exportação", f"Ocorreu um erro ao exportar para Excel: {e}")
-            
 
     def delete_product(self):
         id_or_name = self.delete_id_entry.get().strip()
@@ -229,6 +212,42 @@ class InventoryView(ctk.CTk):
 
         self.delete_window.destroy()
         self.create_manage_inventory_screen()
+
+    def show_remove_product_window(self):
+        self.remove_window = ctk.CTkToplevel(self)
+        self.remove_window.title("Retirar Produto")
+        self.remove_window.geometry("400x200+{}+{}".format(self.winfo_x() + self.winfo_width() // 2 - 200, self.winfo_y() + self.winfo_height() // 2 - 100))
+        self.remove_window.transient(self)
+        self.remove_window.grab_set()
+
+        self.remove_id_label = ctk.CTkLabel(self.remove_window, text="ID do Produto:")
+        self.remove_id_label.pack(pady=5)
+        self.remove_id_entry = ctk.CTkEntry(self.remove_window)
+        self.remove_id_entry.pack(pady=5)
+
+        self.remove_quantity_label = ctk.CTkLabel(self.remove_window, text="Quantidade a Retirar:")
+        self.remove_quantity_label.pack(pady=5)
+        self.remove_quantity_entry = ctk.CTkEntry(self.remove_window)
+        self.remove_quantity_entry.pack(pady=5)
+
+        self.remove_confirm_button = ctk.CTkButton(self.remove_window, text="Confirmar", command=self.remove_product)
+        self.remove_confirm_button.pack(pady=10)
+
+    def show_delete_product_window(self):
+        self.delete_window = ctk.CTkToplevel(self)
+        self.delete_window.title("Excluir Produto")
+        self.delete_window.geometry("400x150+{}+{}".format(self.winfo_x() + self.winfo_width() // 2 - 200, self.winfo_y() + self.winfo_height() // 2 - 75))
+        self.delete_window.transient(self)
+        self.delete_window.grab_set() 
+
+        self.delete_id_label = ctk.CTkLabel(self.delete_window, text="ID do Produto:")
+        self.delete_id_label.pack(pady=5)
+        self.delete_id_entry = ctk.CTkEntry(self.delete_window)
+        self.delete_id_entry.pack(pady=5)
+
+        self.delete_confirm_button = ctk.CTkButton(self.delete_window, text="Confirmar", command=self.delete_product)
+        self.delete_confirm_button.pack(pady=10)
+
     def clear_screen(self):
         for widget in self.winfo_children():
             widget.destroy()
